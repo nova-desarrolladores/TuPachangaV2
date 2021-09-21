617e76081cf019c52fe56de8b723607ed84b10eb
Jonathan Nava <jonathan.navaflores@gmail.com>
2021-09-21 14:50:19 -0500

    add model servicio

---
+++ models/servicio.js
@@ -0,0 +1,46 @@
+// Modelo o Esquema tabla usuarios para la base de datos
+
+const {Schema, model} = require('mongoose');
+
+const ServicioSchema = Schema({
+    nombreServicio: {
+        type: String,
+        required: [true, 'El nombre del servicio es obligatorio'],
+    },
+    descripcion: {
+        type: String,
+        required: [true, 'La descripción del servicio es obligatoria'],  
+    },
+    tipoServicio: {
+        type: String,
+        required: [true, 'El tipo de servicio es obligatorio'],  
+    },
+    imagenServicio: {
+        type: String,
+    },
+    galeriaImagen: {
+        type: String,
+    },
+    estadoServicio:{
+        type: Boolean,
+        default: true
+    },
+    fechaPublicacion: {
+        type: Date,  
+    }, 
+    precio: {
+        type: Double,
+        required: [true, 'El precio del servicio es obligatorio'],
+
+    },    
+});
+
+
+// Metodos para sobre escribir metodos
+// UsuarioSchema.methods.toJSON = function (){
+//     const {password, __v, ...usuario} = this.toObject();
+//     return usuario;
+// }
+
+// Exportar el modelo (<nombre coleccion>, <esquema creado>)
+module.exports = model('Servicio', ServicioSchema);
