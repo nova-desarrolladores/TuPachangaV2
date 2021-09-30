f6e4bbdea52b763659cf75edda0a5805ba156a37
Jonathan Nava <jonathan.navaflores@gmail.com>
2021-09-30 16:18:43 -0500

    update database config

--- database/config.js
+++ database/config.js
@@ -1,28 +1,3 @@
-
-// Cargar modulo mongoose 
-const mongoose =  require('mongoose');
-
-// Funcion Conexion a la base de datos de MongoDB
-const dbConnection = async() => {
-    try {
-       await mongoose.connect(process.env.MONGO_CNN, {
-           useNewUrlParser: true,
-           useUnifiedTopology: true,
-           useCreateIndex: true,
-           useFindAndModify: false,
-           
-        });
-        console.log('Base de datos conectada correctamente');
-    } catch (error) {
-        console.log(error);
-        throw new Error('Error! No se pudo conectar a la base de datos');
-    }
-}
-
-// Exportacion del modulo de la base de datos
-module.exports = {
-    dbConnection,
-
 // Cargar modulo mongoose 
 const mongoose =  require('mongoose');
 
